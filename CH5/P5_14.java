2004d7e519b24f4f86af63dcb5a90ccb63e8dfa9
agmui <anthonynmui@gmail.com>
2020-11-15 14:57:21 -0800

    finnised CH5

--- CH5/P5_14.java
+++ CH5/P5_14.java
@@ -8,6 +8,24 @@ public class P5_14 {
         double x = new Scanner(System.in).nextDouble();
         System.out.print("input pow: ");
         int p = new Scanner(System.in).nextInt();
-        //if ()
+        if (Math.pow(x,p)>Math.pow(10,-1)){
+            System.out.println("Radio Waves");
+        } else if (Math.pow(x,p)<=Math.pow(10,-1)&&Math.pow(x,p)>Math.pow(10,-3)){
+            System.out.println("Microwaves");
+
+        } else if (Math.pow(x,p)<=Math.pow(10,-3)&&Math.pow(x,p)>7*Math.pow(10,-7)){
+            System.out.println("Infrared");
+
+        } else if (Math.pow(x,p)<=7*Math.pow(10,-7)&&Math.pow(x,p)>4*Math.pow(10,-7)){
+            System.out.println("Visible light");
+
+        } else if (Math.pow(x,p)<=4*Math.pow(10,-7)&&Math.pow(x,p)>Math.pow(10,-8)){
+            System.out.println("Ultraviolet");
+
+        } else if (Math.pow(x,p)<=Math.pow(10,-8)&&Math.pow(x,p)>Math.pow(10,-11)){
+            System.out.println("X-rays");
+        } else{
+            System.out.println("Gamma rays");
+        }
     }
 }
