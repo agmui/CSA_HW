65bb217069497a4b8c91dc56b7cc18870de33d22
agmui <65274668+agmui@users.noreply.github.com>
2020-12-11 00:41:53 -0800

    Added CH6

---
+++ CH6/E6_4.java
@@ -0,0 +1,14 @@
+package CH6;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class E6_4 {
+    public static void main(String[] args) {
+        //int[] x = {};
+        List<Integer> l = new ArrayList<>();
+
+        l.add(1);
+        System.out.println(l);
+    }
+}
