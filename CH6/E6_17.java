f4ad9283c01f476fa82a9af32eab009d653deb95
agmui <65274668+agmui@users.noreply.github.com>
2020-12-11 13:45:43 -0800

    Finished ch6 hw

---
+++ CH6/E6_17.java
@@ -0,0 +1,12 @@
+package CH6;
+
+public class E6_17 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 10+1; i++) {
+            for (int j = 1; j < 10+1; j++) {
+                System.out.print(i*j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
