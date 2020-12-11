b001bcf242b8daceaded90e8290d2cd1501ef6f2
agmui <65274668+agmui@users.noreply.github.com>
2020-12-11 11:14:52 -0800

    Finished 6_4

--- CH6/E6_4.java
+++ CH6/E6_4.java
@@ -1,26 +1,32 @@
 package CH6;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class E6_4 {
 
     public static void main(String[] args) {
-        List<String> l = new ArrayList<String>(Arrays.asList("a", "e", "i", "o", "u"));
-        //String input = new Scanner(System.in).next();
-        String input = "AaeioubB";
+        int count = 0;
+        List<String> l = new ArrayList<String>(Arrays.asList("a", "e", "i", "o", "u", "A", "E", "I", "O", "U"));
+        List<Integer> l2 = new ArrayList<>();
+        String input = new Scanner(System.in).next();
+        //String input = "AaeioubB";
         char[] ch = input.toCharArray();
         for (int i = 0; i < ch.length; i++){
-            i++;
             //System.out.println(c);
             if (Character.isUpperCase(ch[i]) || i%2==0){
                 System.out.println(ch[i]);
             }
-            if (l.contains(ch[i])){
-              ch[i] = '_';
+
+            if (l.contains(Character.toString(ch[i]))){
+                ch[i]='_';
+                count++;
+                l2.add(i);
             }
         }
-        System.out.println(ch);
+        System.out.println(Arrays.toString(ch)+","+count+","+l2);
     }
 }
