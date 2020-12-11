48bcb635780e0329bba4fdf071a962129ae4ed7e
agmui <65274668+agmui@users.noreply.github.com>
2020-12-11 10:21:07 -0800

    added 6_4

--- CH6/E6_4.java
+++ CH6/E6_4.java
@@ -2,13 +2,19 @@ package CH6;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class E6_4 {
     public static void main(String[] args) {
-        //int[] x = {};
-        List<Integer> l = new ArrayList<>();
-
-        l.add(1);
-        System.out.println(l);
+        int i = 0;
+        String input = new Scanner(System.in).next();
+        char[] ch = input.toCharArray();
+        for (char c : ch){
+            i++;
+            //System.out.println(c);
+            if (Character.isUpperCase(c)||i%2==0){
+                System.out.println(c);
+            }
+        }
     }
 }
