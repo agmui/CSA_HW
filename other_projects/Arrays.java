1744c6b8aba4d14335a289b60ea629198d11ed7b
agmui <65274668+agmui@users.noreply.github.com>
2020-12-17 10:12:44 -0800

    Added other projects folder

--- other_projects/Arrays.java
+++ other_projects/Arrays.java
@@ -4,14 +4,22 @@ import java.util.Scanner;
 
 public class Arrays{
   public static void main(String[] args) {
-    System.out.print("input: ");
+    System.out.print("How many days' temperatures? ");
     int x = new Scanner(System.in).nextInt();
     int[] temp = new int[x];
+    float y = 0;
+    int z = 0;
     for (int i = 0; i < x; i++) {
-      System.out.print("input day "+i+": ");
+      System.out.print("Day "+(i+1)+"'s high temp: ");
       temp[i] = new Scanner(System.in).nextInt();
+      y += temp[i];
     }
-    System.out.print("temp");
-
+    for (int i : temp) {
+      if(i > (y/x)){
+        z++;
+      }
+    }
+    System.out.println("Average temp = "+(y/x));
+    System.out.println(z + " days were above average");
   }
 }
