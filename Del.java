5c909a265c6e1fe1b1aac2baf13449db44d0dc0a
agmui <65274668+agmui@users.noreply.github.com>
2021-02-22 09:16:31 -0800

    made small correctiosn

--- Del.java
+++ Del.java
@@ -1,5 +1,21 @@
 public class Del{
   public static void main(String[] args) {
-    System.out.println(1*1/2.0);
+     int[] l = {1,2,3,4,5};
+      int n = l[1];
+      int comp=1;//index of num
+      for (int i = 0; i < l.length-1; i++) {
+          comp ++;
+          System.out.println(n==l[comp]);
+          if (comp>l.length){
+             comp = 0;
+          }
+          System.out.println(comp);
+      }
+     /*int n = l[0];
+      for (int i: l) {
+         if (n == i){
+             System.out.println(1);
+         }
+      }*/
   }
 }
--- other_projects/CH9Practice/Employee.java
+++ other_projects/CH9Practice/Employee.java
@@ -8,8 +8,8 @@ Shreay Chigurupati
 package other_projects.CH9Practice;
 
 public class Employee {
-    public final String name;
-    public double salary;
+    private final String name;
+    private double salary;
     public Employee(String name, double salary){
        this.name = name;
        this.salary = salary;
@@ -26,10 +26,9 @@ public class Employee {
     public void bonus(){
        this.salary *= 1.1;
     }
-    public boolean equals(String name, double salary){
-        return (!(name.equals(this.name) && (salary == this.salary)));
+    public boolean equals(Employee e){
+        return ((e.name.equals(this.name) && (e.salary == this.salary)));
     }
-
     @Override
     public String toString() {
         return "Employee{" +
@@ -68,6 +67,11 @@ class EmployeeTester{
             System.out.println(i.getName() + i.getSalary());
             i.setSalary(2);
             i.bonus();
+            for (Employee j: l) {
+               if(j.equals(i)){
+                   System.out.println(11);
+               }
+            }
             System.out.println((i.equals(l[0])));
             System.out.println(i.toString());
         }
@@ -78,12 +82,12 @@ class EmployeeTester{
         Employee e2 = new Employee("e2", 1);
         Employee e3 = new Employee("e3", 1);
         Employee[] l = {e1, e1_cp, e2, e3};
-        //System.out.println((e1_cp.equals(e1)));
         test(l);
         Manager m1 = new Manager("m1", 1, "1");
+        Manager m1_cp = new Manager("m1", 1, "1");
         Manager m2 = new Manager("m2", 1, "1");
         Manager m3 = new Manager("m3", 1, "1");
-        Manager[] l2 = {m1, m2, m3};
+        Manager[] l2 = {m1, m1_cp, m2, m3};
         test(l2);
         for (Manager i: l2) {
             System.out.println(i.getId());
