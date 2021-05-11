d77486c4daff3c8098fc05593d10fb80368c19fc
agmui <65274668+agmui@users.noreply.github.com>
2021-05-11 09:32:06 -0700

    finished CH 7

--- src/AP_FRQ/CH_7/UserName.java
+++ src/AP_FRQ/CH_7/UserName.java
@@ -1,24 +1,30 @@
+package AP_FRQ.CH_7;
+
 import java.util.*;
 public class UserName{
-  private ArrayList<String> possibleNames = new ArrayList<String>();
-  public UserName(String firstName, String lastName)  {
-    for(int i = 1; i < firstName.length()+1; i++){
-      possibleNames.add(lastName+firstName.substring(0,i));
-    }
-  }
-  public boolean isUsed(String name, String[] arr){
-      return possibleNames.contains(arr);
-  }
-  public void setAvailableUserNames(String[] usedNames){
-    for (String i : usedNames) {
-      if (this.isUsed("hi", usedNames)) {
-        possibleNames.remove(i);
-      System.out.println(i);}
-    }
-  }
-   public static void main(String[] args) {
-     UserName test = new UserName("firstName", "lastName");
-     String[] used = {"harta", "hartm", "harty"};	
-     test.setAvailableUserNames(used);
-   }
+      private ArrayList<String> possibleNames = new ArrayList<String>();
+      public UserName(String firstName, String lastName)  {
+          for(int i = 1; i < firstName.length()+1; i++){
+              possibleNames.add(lastName+firstName.substring(0,i));
+          }
+      }
+      public boolean isUsed(String name, String[] arr){
+          for (String i : arr){
+              if(i.equals(name)) return true;
+          }
+          return false;
+      }
+      public void setAvailableUserNames(String[] usedNames){
+          for (int i = 0; i < possibleNames.size(); i++) {
+              if (this.isUsed(possibleNames.get(i), usedNames)) {
+                  System.out.println(possibleNames.get(i));
+                  possibleNames.remove(possibleNames.get(i));
+              }
+          }
+      }
+       public static void main(String[] args) {
+          UserName test = new UserName("mary", "hart");
+          String[] used = {"harta", "hartm", "harty"};
+          test.setAvailableUserNames(used);
+      }
 }
